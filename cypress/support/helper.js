3d7760a359c36933d855ccedae72142f7af27c29
abhidp <abhi358@gmail.com>
2019-11-10 21:01:25 +1100

    createEmployee

---
+++ cypress/support/helper.js
@@ -0,0 +1,16 @@
+export function randomString(i) {
+  return [...Array(i)].map(i => (~~(Math.random() * 36)).toString(36)).join('')
+}
+
+
+export function randomDate() {
+  const date = new Date(+(new Date()) - Math.floor(Math.random() * 1000000000000))
+  return date.toISOString().split('T')[0]
+}
+
+export var emp = {
+  firstName: `${randomString(5)}`,
+  lastName: `${randomString(7)}`,
+  email : `${randomString(3)}.${randomString(4)}@cafetownsend.com`,
+  date : `${randomDate()}`
+} 
